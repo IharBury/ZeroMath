6be41bd2219da861b58b42d620277bab191b5930
Ihar Bury <Ihar.Bury@gmail.com>
2026-06-05 20:33:29 +0200

    Enable decidable cardinal Peano less-than

--- ZeroMath/Numbers/CardinalNatural/Peano.lean
+++ ZeroMath/Numbers/CardinalNatural/Peano.lean
@@ -685,6 +685,11 @@ theorem isLessThan_eq_false_iff_not_lt (a b : Peano) : Peano.isLessThan a b = fa
     · have h3 := (isLessThan_eq_true_iff_lt a b).mp h2
       contradiction
 
+instance decidableLessThan (a b : Peano) : Decidable (a < b) := by
+  cases h : Peano.isLessThan a b with
+  | false => exact isFalse ((isLessThan_eq_false_iff_not_lt a b).mp h)
+  | true => exact isTrue ((isLessThan_eq_true_iff_lt a b).mp h)
+
 theorem le_trans {a b c : Peano} (hab : a ≤ b) (hbc : b ≤ c) : a ≤ c := by
   cases hab with
   | inl hab_lt =>
